5e931ad660b8c76071f60ba3deeecc579a43d252
Kun Xiang <evolve1989@gmail.com>
2019-11-05 10:06:37 -0500

    remove comments

---
+++ python-fasta.py
@@ -0,0 +1,18 @@
+
+
+def read_fasta(filename):
+    seq =''
+    f=open(filename)
+    for line in f:
+        line = line.strip()
+        if not '>' in line:
+            seq=seq+line
+    f.close()
+    return seq
+
+print(read_fasta('ae.fa'))
+
+
+
+
+
